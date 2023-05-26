bf6536b4f07788a9ed94dced153ee439106ce456
HoangNgocThang <thanghai.tb123@gmail.com>
2023-05-26 13:53:40 +0700

    test show img

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
 import * as Sentry from "@sentry/react-native";
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, Image } from 'react-native';
 import codePush from "react-native-code-push";
+const img = require('./images/callapi.png');
 
 Sentry.init({
   dsn: "https://0b1bfb681f09438ab2021e3eb26ff6b4@o4504726892969984.ingest.sentry.io/4504726894411776",
@@ -21,17 +14,18 @@ Sentry.init({
 
 const App = () => {
 
-  React.useEffect(()=> {
+  React.useEffect(() => {
     // open cmt test sentry
     // throw new Error("Thang 111 My first Sentry error!");
-  },[])
+  }, [])
 
   return (
     <SafeAreaView>
       <ScrollView>
         <Text>TEST CODE PUSH2</Text>
-        <Text style={{color:'red'}}>Making my way</Text>
+        <Text style={{ color: 'red' }}>Making my way</Text>
         <Text>Hello Anh em</Text>
+        <Image source={img} style={{ width: 100, height: 100 }} />
       </ScrollView>
     </SafeAreaView>
   );
